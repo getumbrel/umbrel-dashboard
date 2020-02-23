6c34a075e0fdfdf9392cddb5f3ea036ea79c35e2
Mayank <mayankchhabra9@gmail.com>
2020-02-23 16:44:27 +0530

    widget cards

---
+++ src/components/CardWidget.vue
@@ -0,0 +1,56 @@
+<template>
+  <b-card header-tag="header" footer-tag="footer" no-body>
+    <!-- <template v-slot:header></template> -->
+    <div>
+      <div class="card-custom-header p-4">
+        <div class="d-flex w-100 justify-content-between">
+          <h6 class="mb-0 font-weight-normal text-muted">{{ header }}</h6>
+          <small class="text-success">
+            <svg
+              width="8"
+              height="8"
+              viewBox="0 0 8 8"
+              fill="none"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <circle cx="4" cy="4" r="4" fill="#00CD98" />
+            </svg>
+            {{ status }}
+          </small>
+        </div>
+      </div>
+      <div class="card-custom-body">
+        <div class="card-app-info px-4">
+          <div class="d-flex w-100 justify-content-between mb-4">
+            <div>
+              <h3 class="mb-1">{{ title }}</h3>
+              <p class="text-muted mb-0">{{ subTitle }}</p>
+            </div>
+            <img :alt="header" :src="require(`@/assets/${icon}`)" />
+          </div>
+        </div>
+        <slot></slot>
+      </div>
+    </div>
+    <!-- <template v-slot:footer></template> -->
+  </b-card>
+</template>
+
+<script>
+export default {
+  data() {},
+  props: {
+    header: String,
+    status: String,
+    statusType: String,
+    title: String,
+    subTitle: String,
+    icon: String
+  },
+  computed: {},
+  methods: {}
+};
+</script>
+
+<style lang="scss" scoped>
+</style>
--- src/views/Dashboard.vue
+++ src/views/Dashboard.vue
@@ -7,75 +7,59 @@
       </div>
       <b-row>
         <b-col col sm="4">
-          <b-card header-tag="header" footer-tag="footer" no-body>
-            <!-- <template v-slot:header></template> -->
-            <div>
-              <div class="card-custom-header p-4">
-                <div class="d-flex w-100 justify-content-between">
-                  <h6 class="mb-0 font-weight-normal text-muted">Bitcoin Core</h6>
-                  <small class="text-success">
-                    <svg
-                      width="8"
-                      height="8"
-                      viewBox="0 0 8 8"
-                      fill="none"
-                      xmlns="http://www.w3.org/2000/svg"
-                    >
-                      <circle cx="4" cy="4" r="4" fill="#00CD98" />
-                    </svg>
-                    Running
-                  </small>
-                </div>
+          <card-widget
+            header="Lightning Network"
+            status="Running"
+            status-type="success"
+            title="162,500"
+            sub-title="Sats"
+            icon="icon-app-lightning.svg"
+          ></card-widget>
+        </b-col>
+        <b-col col sm="4">
+          <card-widget
+            header="Bitcoin Core"
+            status="Running"
+            status-type="success"
+            title="100%"
+            sub-title="Synchronized"
+            icon="icon-app-bitcoin.svg"
+          >
+            <div class="px-4">
+              <div class="d-flex w-100 justify-content-between">
+                <p class="mb-1">Connected Peers</p>
+                <p>8</p>
               </div>
-              <div class="card-custom-body">
-                <div class="card-app-info px-4">
-                  <div class="d-flex w-100 justify-content-between mb-4">
-                    <div>
-                      <h3 class="mb-1">100%</h3>
-                      <p class="text-muted mb-0">Synchronized</p>
-                    </div>
-                    <img alt="Bitcoin Core" src="@/assets/icon-app-bitcoin.svg" />
-                  </div>
-                </div>
-                <div class="px-4">
+              <p>Latest Blocks</p>
+              <b-list-group>
+                <b-list-group-item href="#" class="flex-column align-items-start">
                   <div class="d-flex w-100 justify-content-between">
-                    <p class="mb-1">Connected Peers</p>
-                    <p>8</p>
+                    <h5 class="mb-1 font-weight-normal">Block 618555</h5>
+                    <small class="text-muted">7 mins ago</small>
                   </div>
-                  <p>Latest Blocks</p>
-                  <b-list-group>
-                    <b-list-group-item href="#" class="flex-column align-items-start">
-                      <div class="d-flex w-100 justify-content-between">
-                        <h5 class="mb-1 font-weight-normal">Block 618555</h5>
-                        <small class="text-muted">7 mins ago</small>
-                      </div>
-                      <small class="text-muted">1708 transactions</small>
-                    </b-list-group-item>
-                    <b-list-group-item href="#" class="flex-column align-items-start">
-                      <div class="d-flex w-100 justify-content-between">
-                        <h5 class="mb-1 font-weight-normal">Block 618554</h5>
-                        <small class="text-muted">18 mins ago</small>
-                      </div>
-                      <small class="text-muted">1893 transactions</small>
-                    </b-list-group-item>
-                    <b-list-group-item href="#" class="flex-column align-items-start">
-                      <div class="d-flex w-100 justify-content-between">
-                        <h5 class="mb-1 font-weight-normal">Block 618553</h5>
-                        <small class="text-muted">32 mins ago</small>
-                      </div>
-                      <small class="text-muted">1680 transactions</small>
-                    </b-list-group-item>
-                  </b-list-group>
-                </div>
-              </div>
-              <div class="card-custom-footer px-4 pt-4 pb-3">
+                  <small class="text-muted">1708 transactions</small>
+                </b-list-group-item>
+                <b-list-group-item href="#" class="flex-column align-items-start">
+                  <div class="d-flex w-100 justify-content-between">
+                    <h5 class="mb-1 font-weight-normal">Block 618554</h5>
+                    <small class="text-muted">18 mins ago</small>
+                  </div>
+                  <small class="text-muted">1893 transactions</small>
+                </b-list-group-item>
+                <b-list-group-item href="#" class="flex-column align-items-start">
+                  <div class="d-flex w-100 justify-content-between">
+                    <h5 class="mb-1 font-weight-normal">Block 618553</h5>
+                    <small class="text-muted">32 mins ago</small>
+                  </div>
+                  <small class="text-muted">1680 transactions</small>
+                </b-list-group-item>
+              </b-list-group>
+              <div class="px-4 pt-4 pb-3">
                 <a href="#" class="card-link">Manage</a>
               </div>
             </div>
-            <!-- <template v-slot:footer></template> -->
-          </b-card>
+          </card-widget>
         </b-col>
-        <b-col col sm="4"></b-col>
       </b-row>
     </div>
   </authenticated-nav>
@@ -84,6 +68,7 @@
 <script>
 // import Vue from "vue";
 import AuthenticatedNav from "@/layouts/AuthenticatedNav";
+import CardWidget from "@/components/CardWidget";
 
 export default {
   data() {
@@ -96,7 +81,8 @@ export default {
   },
   methods: {},
   components: {
-    AuthenticatedNav
+    AuthenticatedNav,
+    CardWidget
   }
 };
 </script>
