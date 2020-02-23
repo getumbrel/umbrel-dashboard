e29359753bf2e43fb2c75dd957e8977d52615cd3
Mayank <mayankchhabra9@gmail.com>
2020-02-23 23:45:05 +0530

    lightning send/receive buttons

--- src/views/Dashboard.vue
+++ src/views/Dashboard.vue
@@ -8,7 +8,7 @@
       <b-row>
         <b-col col sm="4">
           <card-widget
-            header="Lightning Network"
+            header="Lightning Wallet"
             status="Running"
             status-type="success"
             title="162,500"
@@ -82,11 +82,53 @@
                 <small class="text-muted mt-0" style="margin-left: 24px;">1 day ago</small>
               </b-list-group-item>
             </b-list-group>
-            <div class="px-4 py-2">
+            <!-- <div class="px-4 py-2">
               <a href="#" class="card-link">View all transactions</a>
-            </div>
+            </div>-->
             <div class="px-4 pt-2 pb-3">
               <a href="#" class="card-link">Manage</a>
+            </div>
+            <div class>
+              <b-button-group class="w-100">
+                <b-button
+                  class="w-50"
+                  variant="primary"
+                  style="border-radius: 0; border-bottom-left-radius: 1rem; padding-top: 1rem; padding-bottom: 1rem;"
+                >
+                  <svg
+                    width="19"
+                    height="19"
+                    viewBox="0 0 19 19"
+                    fill="none"
+                    xmlns="http://www.w3.org/2000/svg"
+                    class="mr-1"
+                  >
+                    <path
+                      d="M7.06802 4.71946C6.76099 4.71224 6.50825 4.96178 6.50627 5.27413C6.50435 5.57592 6.7539 5.82865 7.05534 5.83022L12.7162 5.86616L4.81508 13.3568C4.59632 13.5735 4.59981 14.1376 4.81615 14.3568C5.03249 14.5759 5.59723 14.572 5.81634 14.3556L13.4988 6.6587L13.4576 12.3143C13.4609 12.6214 13.7108 12.8745 14.0122 12.876C14.3246 12.878 14.5777 12.6281 14.574 12.3214L14.6184 5.32036C14.6257 5.01333 14.3761 4.76059 14.0694 4.76427L7.06802 4.71946Z"
+                      fill="#FFFFFF"
+                    />
+                  </svg>Send
+                </b-button>
+                <b-button
+                  class="w-50"
+                  variant="success"
+                  style="border-radius: 0; border-bottom-right-radius: 1rem; padding-top: 1rem; padding-bottom: 1rem;"
+                >
+                  <svg
+                    width="18"
+                    height="18"
+                    viewBox="0 0 18 18"
+                    fill="none"
+                    xmlns="http://www.w3.org/2000/svg"
+                    class="mr-1"
+                  >
+                    <path
+                      d="M13.5944 6.04611C13.6001 5.73904 13.3493 5.48755 13.0369 5.48712C12.7351 5.4867 12.4836 5.7375 12.4836 6.03895L12.4758 11.6999L4.94598 3.83615C4.72819 3.61848 4.16402 3.62477 3.94599 3.8422C3.72796 4.05963 3.73466 4.62433 3.95209 4.84236L11.6871 12.4864L6.03143 12.4733C5.72435 12.4782 5.47251 12.7293 5.47244 13.0308C5.47201 13.3431 5.72317 13.595 6.0299 13.5898L13.031 13.5994C13.3381 13.6051 13.5896 13.3543 13.5844 13.0476L13.5944 6.04611Z"
+                      fill="#FFFFFF"
+                    />
+                  </svg>Receive
+                </b-button>
+              </b-button-group>
             </div>
           </card-widget>
         </b-col>
@@ -100,29 +142,29 @@
             icon="icon-app-bitcoin.svg"
           >
             <div class>
-              <div class="d-flex w-100 justify-content-between px-4">
+              <!-- <div class="d-flex w-100 justify-content-between px-4">
                 <p class="mb-1">Connected Peers</p>
                 <p>8</p>
-              </div>
+              </div>-->
               <!-- <p class="px-4">Latest Blocks</p> -->
               <b-list-group>
                 <b-list-group-item href="#" class="flex-column align-items-start px-4">
                   <div class="d-flex w-100 justify-content-between">
-                    <h5 class="mb-1 font-weight-normal">Block 618555</h5>
+                    <h6 class="mb-1 font-weight-normal">Block 618555</h6>
                     <small class="text-muted">7 mins ago</small>
                   </div>
                   <small class="text-muted">1708 transactions</small>
                 </b-list-group-item>
                 <b-list-group-item href="#" class="flex-column align-items-start px-4">
                   <div class="d-flex w-100 justify-content-between">
-                    <h5 class="mb-1 font-weight-normal">Block 618554</h5>
+                    <h6 class="mb-1 font-weight-normal">Block 618554</h6>
                     <small class="text-muted">18 mins ago</small>
                   </div>
                   <small class="text-muted">1893 transactions</small>
                 </b-list-group-item>
                 <b-list-group-item href="#" class="flex-column align-items-start px-4">
                   <div class="d-flex w-100 justify-content-between">
-                    <h5 class="mb-1 font-weight-normal">Block 618553</h5>
+                    <h6 class="mb-1 font-weight-normal">Block 618553</h6>
                     <small class="text-muted">32 mins ago</small>
                   </div>
                   <small class="text-muted">1680 transactions</small>
