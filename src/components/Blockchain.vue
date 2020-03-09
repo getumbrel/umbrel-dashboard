f9d144e16d6ddb8349dd8e893897136cf5b8d387
Mayank <mayankchhabra9@gmail.com>
2020-03-09 22:59:43 +0530

    animated blockchain

--- src/components/Blockchain.vue
+++ src/components/Blockchain.vue
@@ -1,49 +1,51 @@
 <template>
   <div>
-    <!-- <b-button class="ml-4 mb-4" @click="addBlocks">+</b-button> -->
-    <ul class="blockchain-container">
-      <li
-        href="#"
-        class="flex-column align-items-start px-4 blockchain-block"
-        v-for="block in blocks"
-        :key="block.number"
-      >
-        <div class="d-flex w-100 justify-content-between">
-          <div class="d-flex">
-            <div class="blockchain-block-icon">
-              <svg
-                width="28"
-                height="30"
-                viewBox="0 0 28 30"
-                fill="none"
-                xmlns="http://www.w3.org/2000/svg"
-              >
-                <path
-                  d="M23.3472 8.84298C24.6806 8.07317 24.6806 6.14865 23.3472 5.37886L15.0304 0.577324C14.4116 0.220073 13.6493 0.220077 13.0305 0.577333L4.71387 5.37887C3.38053 6.14866 3.38052 8.07316 4.71384 8.84297L13.0304 13.6447C13.6493 14.0019 14.4117 14.0019 15.0305 13.6447L23.3472 8.84298Z"
-                  fill="#5351FB"
-                />
-                <path
-                  d="M14.9243 26.5359C14.9243 28.0755 16.591 29.0378 17.9243 28.268L26.2443 23.4644C26.8631 23.1072 27.2443 22.4469 27.2443 21.7324V12.1218C27.2443 10.5822 25.5777 9.61992 24.2444 10.3897L15.9243 15.1931C15.3055 15.5504 14.9243 16.2106 14.9243 16.9252V26.5359Z"
-                  fill="#5351FB"
-                />
-                <path
-                  d="M3.8164 10.3897C2.48306 9.61995 0.816406 10.5822 0.816406 12.1218V21.7324C0.816406 22.4469 1.1976 23.1072 1.81639 23.4644L10.1362 28.268C11.4695 29.0378 13.1362 28.0755 13.1362 26.5359V16.9252C13.1362 16.2106 12.755 15.5504 12.1362 15.1931L3.8164 10.3897Z"
-                  fill="#5351FB"
-                />
-              </svg>
+    <!-- <b-button class="ml-4 mb-4" @click="addFakeBlock">+</b-button> -->
+    <div class="blockchain-container">
+      <transition-group name="blockchain" tag="ul">
+        <li
+          href="#"
+          class="flex-column align-items-start px-4 blockchain-block"
+          v-for="block in blocks"
+          :key="block.number"
+        >
+          <div class="d-flex w-100 justify-content-between">
+            <div class="d-flex">
+              <div class="blockchain-block-icon">
+                <svg
+                  width="28"
+                  height="30"
+                  viewBox="0 0 28 30"
+                  fill="none"
+                  xmlns="http://www.w3.org/2000/svg"
+                >
+                  <path
+                    d="M23.3472 8.84298C24.6806 8.07317 24.6806 6.14865 23.3472 5.37886L15.0304 0.577324C14.4116 0.220073 13.6493 0.220077 13.0305 0.577333L4.71387 5.37887C3.38053 6.14866 3.38052 8.07316 4.71384 8.84297L13.0304 13.6447C13.6493 14.0019 14.4117 14.0019 15.0305 13.6447L23.3472 8.84298Z"
+                    fill="#5351FB"
+                  />
+                  <path
+                    d="M14.9243 26.5359C14.9243 28.0755 16.591 29.0378 17.9243 28.268L26.2443 23.4644C26.8631 23.1072 27.2443 22.4469 27.2443 21.7324V12.1218C27.2443 10.5822 25.5777 9.61992 24.2444 10.3897L15.9243 15.1931C15.3055 15.5504 14.9243 16.2106 14.9243 16.9252V26.5359Z"
+                    fill="#5351FB"
+                  />
+                  <path
+                    d="M3.8164 10.3897C2.48306 9.61995 0.816406 10.5822 0.816406 12.1218V21.7324C0.816406 22.4469 1.1976 23.1072 1.81639 23.4644L10.1362 28.268C11.4695 29.0378 13.1362 28.0755 13.1362 26.5359V16.9252C13.1362 16.2106 12.755 15.5504 12.1362 15.1931L3.8164 10.3897Z"
+                    fill="#5351FB"
+                  />
+                </svg>
 
-              <div class="blockchain-block-icon-chainlink"></div>
-              <div class="blockchain-block-icon-bg"></div>
+                <div class="blockchain-block-icon-chainlink"></div>
+                <div class="blockchain-block-icon-bg"></div>
+              </div>
+              <div class="align-self-center">
+                <h6 class="mb-1 font-weight-normal">Block {{ block.number.toLocaleString() }}</h6>
+                <small class="text-muted">{{ block.txs.toLocaleString() }} transactions</small>
+              </div>
             </div>
-            <div class="align-self-center">
-              <h6 class="mb-1 font-weight-normal">Block {{ block.number }}</h6>
-              <small class="text-muted">{{ block.txs }} transactions</small>
-            </div>
+            <small class="text-muted align-self-center">{{ block.timestamp }}</small>
           </div>
-          <small class="text-muted align-self-center">{{ block.time }}</small>
-        </div>
-      </li>
-    </ul>
+        </li>
+      </transition-group>
+    </div>
   </div>
 </template>
 
@@ -53,35 +55,46 @@ export default {
     return {
       blocks: [
         {
-          number: 683500,
-          txs: 1802,
-          time: "a few secs ago"
+          number: 683500, //block number
+          txs: 1802, //num of txs in block
+          timestamp: "a few secs ago" //block's mining timestamp
         },
         {
           number: 683499,
           txs: 1934,
-          time: "10 mins ago"
+          timestamp: "10 mins ago"
         },
         {
           number: 683498,
           txs: 1783,
-          time: "17 mins ago"
+          timestamp: "17 mins ago"
         },
         {
           number: 683497,
           txs: 1723,
-          time: "27 mins ago"
+          timestamp: "27 mins ago"
         }
       ]
     };
   },
   computed: {},
   methods: {
-    addBlocks() {
+    addFakeBlock() {
       const tip = this.blocks[0];
-      const newBlock = { number: tip.number + 1, txs: tip.txs + 7 };
+      const newBlock = {
+        number: tip.number + 1,
+        txs: tip.txs + 7,
+        timestamp: "a few secs ago"
+      };
+      // this.blocks = [newBlock, ...this.blocks];
       this.blocks = [newBlock, ...this.blocks].slice(0, 3);
     }
+  },
+  mounted() {
+    //create fake blocks
+    window.setInterval(() => {
+      this.addFakeBlock();
+    }, 5000);
   },
   components: {}
 };
@@ -89,11 +102,28 @@ export default {
 
 <style lang="scss" scoped>
 .blockchain-container {
-  list-style: none;
-  padding: 0;
-  margin: 0;
-  height: 18rem;
+  position: relative;
+  ul {
+    list-style: none;
+    padding: 0;
+    margin: 0;
+    max-height: 18rem;
+  }
   overflow: hidden;
+  &:after {
+    //nice faded white so the discarded blocks don't abruptly cut off
+    content: "";
+    position: absolute;
+    height: 2rem;
+    width: 100%;
+    bottom: 0;
+    left: 0;
+    background-image: linear-gradient(
+      to bottom,
+      rgba(255, 255, 255, 0),
+      rgba(255, 255, 255, 1)
+    );
+  }
 }
 .blockchain-block {
   padding: 0 0 2rem 0;
@@ -131,5 +161,23 @@ export default {
     z-index: 0;
     border-radius: 100%;
   }
+}
+
+.blockchain-block {
+  transition: all 1s;
+}
+.blockchain-enter,
+.blockchain-leave-to {
+  opacity: 0;
+  transform: translateY(2rem);
+}
+
+.blockchain-enter-to,
+.blockchain-leave {
+  opacity: 0;
+  transform: translateY(2rem);
+}
+.blockchain-leave-active {
+  // position: absolute;
 }
 </style>
