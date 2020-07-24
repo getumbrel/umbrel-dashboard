a1e55d017bb2cdcea83aff7c087e9d1d7287c583
Mayank Chhabra <mayankchhabra9@gmail.com>
2020-07-24 17:10:41 +0530

    Disable Tor switches in settings (#145)

--- src/components/ToggleSwitch.vue
+++ src/components/ToggleSwitch.vue
@@ -2,7 +2,9 @@
   <div
     @click="toggle"
     class="toggle"
-    :class="{ 'toggle-off': !state.isOn, 'toggle-on': state.isOn }"
+    :class="{ 'toggle-off': !state.isOn, 'toggle-on': state.isOn, 'toggle-disabled': disabled }"
+    v-b-tooltip.hover.left
+    :title="tooltip"
   >
     <div
       class="toggle-switch justify-content-center"
@@ -24,19 +26,23 @@ export default {
     };
   },
   computed: {},
-  // created() {
-  //   this.state.isOn = this.props.on;
-  // },
   methods: {
     toggle() {
+      if (this.disabled) {
+        return;
+      }
       this.state.isOn = !this.state.isOn;
     }
   },
   props: {
-    // on: {
-    //   type: Boolean,
-    //   default: false
-    // }
+    disabled: {
+      type: Boolean,
+      default: false
+    },
+    tooltip: {
+      type: String,
+      default: ""
+    }
   }
 };
 </script>
@@ -56,6 +62,9 @@ export default {
   &.toggle-on {
     background: var(--success);
     box-shadow: none;
+  }
+  &.toggle-disabled {
+    cursor: not-allowed;
   }
 }
 .toggle-switch {
