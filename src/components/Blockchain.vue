16a5d5ce25b6981d610f6c7b83b209bcf6aa4a81
Mayank <mayankchhabra9@gmail.com>
2020-08-18 07:56:09 +0530

    Prevent validating and valid from being shown at the same time

--- src/components/Blockchain.vue
+++ src/components/Blockchain.vue
@@ -38,7 +38,7 @@
                   <div class="blockchain-block-icon-bg"></div>
                 </div>
                 <div class="align-self-center">
-                  <h6 class="mb-1 font-weight-normal">Block {{ block.height.toLocaleString() }}</h6>
+                  <h6 class="mb-1 font-weight-normal">Block {{ block.height.toLocaleString() }},456</h6>
                   <small class="text-muted" v-if="block.numTransactions">
                     {{
                     block.numTransactions.toLocaleString()
@@ -365,6 +365,7 @@ export default {
     width: 100%;
     height: 100%;
     background: #fff;
+    min-width: 80px;
     opacity: 0;
   }
   &:before {
