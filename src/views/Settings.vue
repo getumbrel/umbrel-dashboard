51b3d5e045b9d01ead2744239e39d311d005d60e
Mayank Chhabra <mayankchhabra9@gmail.com>
2020-09-09 16:28:12 +0530

    Move seed to account card (#209)

--- src/views/Settings.vue
+++ src/views/Settings.vue
@@ -32,37 +32,10 @@
               />
             </svg>
           </template>
-          <b-dropdown-item href="#" v-b-modal.seed-modal>View secret words</b-dropdown-item>
           <b-dropdown-item href="/logs" target="_blank">View system logs</b-dropdown-item>
         </b-dropdown>
       </div>
     </div>
-
-    <b-modal id="seed-modal" centered hide-footer>
-      <template v-slot:modal-header="{ close }">
-        <div class="px-2 px-sm-3 pt-2 d-flex justify-content-between w-100">
-          <h3>secret words</h3>
-          <!-- Emulate built in modal header close button action -->
-          <a href="#" class="align-self-center" v-on:click.stop.prevent="close">
-            <svg
-              width="18"
-              height="18"
-              viewBox="0 0 18 18"
-              fill="none"
-              xmlns="http://www.w3.org/2000/svg"
-            >
-              <path
-                fill-rule="evenodd"
-                clip-rule="evenodd"
-                d="M13.6003 4.44197C13.3562 4.19789 12.9605 4.19789 12.7164 4.44197L9.02116 8.1372L5.32596 4.442C5.08188 4.19792 4.68615 4.19792 4.44207 4.442C4.198 4.68607 4.198 5.0818 4.44207 5.32588L8.13728 9.02109L4.44185 12.7165C4.19777 12.9606 4.19777 13.3563 4.44185 13.6004C4.68592 13.8445 5.08165 13.8445 5.32573 13.6004L9.02116 9.90497L12.7166 13.6004C12.9607 13.8445 13.3564 13.8445 13.6005 13.6004C13.8446 13.3563 13.8446 12.9606 13.6005 12.7165L9.90505 9.02109L13.6003 5.32585C13.8444 5.08178 13.8444 4.68605 13.6003 4.44197Z"
-                fill="#6c757d"
-              />
-            </svg>
-          </a>
-        </div>
-      </template>
-      <seed></seed>
-    </b-modal>
 
     <b-row>
       <b-col col cols="12" md="6" xl="4">
@@ -99,7 +72,7 @@
             <div class="px-3 px-lg-4 mb-4">
               <div class="d-flex justify-content-between w-100 mb-3">
                 <div class="w-75">
-                  <span class="d-block">Remote Access</span>
+                  <span class="d-block">Remote access</span>
                   <small
                     class="d-block"
                     style="opacity: 0.4"
@@ -119,6 +92,42 @@
       </b-col>
       <b-col col cols="12" md="6" xl="4">
         <card-widget header="Account" :loading="isChangingPassword">
+          <div class="pt-0">
+            <div class="d-flex w-100 justify-content-between px-3 px-lg-4 mb-4">
+              <div>
+                <span class="d-block">Secret words</span>
+                <small class="d-block" style="opacity: 0.4">Note down your 24 secret words</small>
+              </div>
+
+              <b-button variant="outline-primary" size="sm" v-b-modal.seed-modal>View</b-button>
+
+              <b-modal id="seed-modal" centered hide-footer>
+                <template v-slot:modal-header="{ close }">
+                  <div class="px-2 px-sm-3 pt-2 d-flex justify-content-between w-100">
+                    <h3>secret words</h3>
+                    <!-- Emulate built in modal header close button action -->
+                    <a href="#" class="align-self-center" v-on:click.stop.prevent="close">
+                      <svg
+                        width="18"
+                        height="18"
+                        viewBox="0 0 18 18"
+                        fill="none"
+                        xmlns="http://www.w3.org/2000/svg"
+                      >
+                        <path
+                          fill-rule="evenodd"
+                          clip-rule="evenodd"
+                          d="M13.6003 4.44197C13.3562 4.19789 12.9605 4.19789 12.7164 4.44197L9.02116 8.1372L5.32596 4.442C5.08188 4.19792 4.68615 4.19792 4.44207 4.442C4.198 4.68607 4.198 5.0818 4.44207 5.32588L8.13728 9.02109L4.44185 12.7165C4.19777 12.9606 4.19777 13.3563 4.44185 13.6004C4.68592 13.8445 5.08165 13.8445 5.32573 13.6004L9.02116 9.90497L12.7166 13.6004C12.9607 13.8445 13.3564 13.8445 13.6005 13.6004C13.8446 13.3563 13.8446 12.9606 13.6005 12.7165L9.90505 9.02109L13.6003 5.32585C13.8444 5.08178 13.8444 4.68605 13.6003 4.44197Z"
+                          fill="#6c757d"
+                        />
+                      </svg>
+                    </a>
+                  </div>
+                </template>
+                <seed></seed>
+              </b-modal>
+            </div>
+          </div>
           <div class="pt-0">
             <div class="d-flex w-100 justify-content-between px-3 px-lg-4 mb-4">
               <div>
