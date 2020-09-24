6d221c2181d467b9a483590958c7195475414742
Mayank <mayankchhabra9@gmail.com>
2020-09-25 00:22:19 +0530

    Reorder alphabetically

--- src/components/LightningConnectWallet.vue
+++ src/components/LightningConnectWallet.vue
@@ -2,38 +2,74 @@
   <div>
     <b-form-select v-model="selectedWallet" class="mb-3">
       <b-form-select-option
-        :value="{walletName: 'Select your wallet', type: null, network: null}"
+        :value="{ walletName: 'Select your wallet', type: null, network: null }"
         disabled
-      >Select your wallet</b-form-select-option>
+        >Select your wallet</b-form-select-option
+      >
       <b-form-select-option
-        :value="{walletName: 'Zap Desktop', type: 'grpc', network: 'tor'}"
-      >Zap Desktop</b-form-select-option>
+        :value="{ walletName: 'Zap Desktop', type: 'grpc', network: 'tor' }"
+        >Zap Desktop</b-form-select-option
+      >
       <b-form-select-option
-        :value="{walletName: 'Zap Mobile', type: 'rest', network: 'tor'}"
-      >Zap Mobile</b-form-select-option>
-      <b-form-select-option :value="{walletName: 'Zeus', type: 'rest', network: 'local'}">Zeus</b-form-select-option>
+        :value="{ walletName: 'Zap Mobile', type: 'rest', network: 'tor' }"
+        >Zap Mobile</b-form-select-option
+      >
+      <b-form-select-option
+        :value="{ walletName: 'Zeus', type: 'rest', network: 'local' }"
+        >Zeus</b-form-select-option
+      >
       <b-form-select-option-group label="Other">
         <b-form-select-option
-          :value="{walletName: 'any other wallet that uses lndconnect REST', type: 'rest', network: 'local'}"
-        >lndconnect REST</b-form-select-option>
+          :value="{
+            walletName: 'any other wallet that uses lndconnect gRPC',
+            type: 'grpc',
+            network: 'local'
+          }"
+          >lndconnect gRPC</b-form-select-option
+        >
         <b-form-select-option
-          :value="{walletName: 'any other wallet that uses lndconnect REST (Tor)', type: 'rest', network: 'tor'}"
-        >lndconnect REST (Tor)</b-form-select-option>
+          :value="{
+            walletName: 'any other wallet that uses lndconnect gRPC (Tor)',
+            type: 'grpc',
+            network: 'tor'
+          }"
+          >lndconnect gRPC (Tor)</b-form-select-option
+        >
         <b-form-select-option
-          :value="{walletName: 'any other wallet that uses lndconnect gRPC', type: 'grpc', network: 'local'}"
-        >lndconnect gRPC</b-form-select-option>
+          :value="{
+            walletName: 'any other wallet that uses lndconnect REST',
+            type: 'rest',
+            network: 'local'
+          }"
+          >lndconnect REST</b-form-select-option
+        >
         <b-form-select-option
-          :value="{walletName: 'any other wallet that uses lndconnect gRPC (Tor)', type: 'grpc', network: 'tor'}"
-        >lndconnect gRPC (Tor)</b-form-select-option>
+          :value="{
+            walletName: 'any other wallet that uses lndconnect REST (Tor)',
+            type: 'rest',
+            network: 'tor'
+          }"
+          >lndconnect REST (Tor)</b-form-select-option
+        >
       </b-form-select-option-group>
     </b-form-select>
-    <div class="d-lg-flex align-items-center" v-if="lndConnectUrl && selectedWallet.type">
+    <div
+      class="d-lg-flex align-items-center"
+      v-if="lndConnectUrl && selectedWallet.type"
+    >
       <!-- Pubkey QR Code -->
-      <qr-code :value="lndConnectUrl" :size="200" level="M" class="qr-image mb-3 mb-lg-0" showLogo></qr-code>
+      <qr-code
+        :value="lndConnectUrl"
+        :size="200"
+        level="M"
+        class="qr-image mb-3 mb-lg-0"
+        showLogo
+      ></qr-code>
       <div class="w-100 align-self-center ml-lg-3">
-        <p
-          class="text-center text-lg-left"
-        >Connect {{ selectedWallet.walletName }} to your Umbrel using the following url</p>
+        <p class="text-center text-lg-left">
+          Connect {{ selectedWallet.walletName }} to your Umbrel using the
+          following url
+        </p>
         <input-copy class="mb-2" size="sm" :value="lndConnectUrl"></input-copy>
       </div>
     </div>
