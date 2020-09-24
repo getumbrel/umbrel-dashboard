015de8806f7af94e13d69b38b05eb0159652db61
Luke Childs <lukechilds123@gmail.com>
2020-09-24 16:22:20 +0700

    Remove extra instances of "Wallet"

--- src/components/BitcoinConnectWallet.vue
+++ src/components/BitcoinConnectWallet.vue
@@ -5,11 +5,11 @@
         :value="{walletName: 'Select your wallet', type: null}"
         disabled
       >Select your wallet</b-form-select-option>
-      <b-form-select-option :value="{walletName: 'Wasabi Wallet', type: 'p2p'}">Wasabi Wallet</b-form-select-option>
-      <b-form-select-option :value="{walletName: 'Electrum Wallet', type: 'electrum'}">Electrum</b-form-select-option>
+      <b-form-select-option :value="{walletName: 'Wasabi', type: 'p2p'}">Wasabi</b-form-select-option>
+      <b-form-select-option :value="{walletName: 'Electrum', type: 'electrum'}">Electrum</b-form-select-option>
       <b-form-select-option :value="{walletName: 'BlueWallet (on-chain only)', type: 'electrum'}">BlueWallet</b-form-select-option>
       <b-form-select-option :value="{walletName: 'Blockstream Green', type: 'p2p'}">Blockstream Green</b-form-select-option>
-      <b-form-select-option :value="{walletName: 'Phoenix Wallet', type: 'electrum'}">Phoenix Wallet</b-form-select-option>
+      <b-form-select-option :value="{walletName: 'Phoenix', type: 'electrum'}">Phoenix</b-form-select-option>
       <b-form-select-option-group label="Other">
         <b-form-select-option
           :value="{walletName: 'any other wallet that can connect to Bitcoin Core\'s P2P port', type: 'p2p'}"
