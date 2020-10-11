9e2bf87f2f10002b8d333401e805dd95cfa2c8b2
Lounès Ksouri <dev@louneskmt.com>
2020-10-11 15:32:20 +0200

    Added "Connect Wallet" view + modified menu in consequences

--- src/router/index.js
+++ src/router/index.js
@@ -13,6 +13,7 @@ import Dashboard from "../views/Dashboard.vue";
 import Bitcoin from "../views/Bitcoin.vue";
 import Lightning from "../views/Lightning.vue";
 import Settings from "../views/Settings.vue";
+import ConnectWallet from "../views/ConnectWallet.vue";
 import Logout from "../views/Logout.vue";
 
 Vue.use(VueRouter);
@@ -91,6 +92,18 @@ const routes = [
             path: "",
             name: "settings",
             component: Settings
+          }
+        ]
+      },
+      {
+        path: "/connect",
+        component: DashboardLayout,
+        meta: { requiresAuth: true },
+        children: [
+          {
+            path: "",
+            name: "connect",
+            component: ConnectWallet
           }
         ]
       },
---
+++ src/views/ConnectWallet.vue
@@ -0,0 +1,61 @@
+<template>
+  <div class="p-sm-2">
+    <div class="my-3 pb-2">
+      <div class="d-flex justify-content-between align-items-center">
+        <h1>connect wallet</h1>
+      </div>
+    </div>
+
+    <b-row class="row-eq-height">
+      <b-col col cols="12" md="6" xl="4">
+        <card-widget header="Wallet">
+
+        </card-widget>
+      </b-col>
+      <b-col col cols="12" md="6" xl="8">
+        <card-widget header="Wallet">
+
+        </card-widget>
+      </b-col>
+    </b-row>
+  </div>
+</template>
+
+<script>
+import { mapState } from "vuex";
+import moment from "moment";
+
+import CardWidget from "@/components/CardWidget";
+
+export default {
+  data() {
+    return {
+    
+    };
+  },
+  computed: {
+    ...mapState({
+      onionAddress: state => state.system.onionAddress
+    })
+  },
+  created() {
+    
+  },
+  methods: {
+    getReadableTime(timestamp) {
+      return moment(timestamp).format("MMM D, h:mm:ss a");
+    }
+  },
+  beforeDestroy() {
+  
+  },
+  watch: {
+    
+  },
+  components: {
+    CardWidget
+  }
+};
+</script>
+
+<style lang="scss" scoped></style>
