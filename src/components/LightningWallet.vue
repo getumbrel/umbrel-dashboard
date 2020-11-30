0ee9c300c3693c3d15826e47b559c837ad0ecf55
Edd Williams <edd.williams@me.com>
2020-11-30 20:25:18 +0000

    Update src/components/LightningWallet.vue
    Co-authored-by: Mayank Chhabra <mayankchhabra9@gmail.com>

--- src/components/LightningWallet.vue
+++ src/components/LightningWallet.vue
@@ -847,7 +847,7 @@ export default {
       // Check that there is enough in the recieve
       if (!this.maxRecieve || this.recieve.amount > this.maxRecieve) {
         const toastOptions = {
-          title: "Low incoming balance",
+          title: "Error creating invoice",
           autoHideDelay: 3000,
           variant: "warning",
           solid: true,
