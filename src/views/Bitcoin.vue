2ebb75e63fba1f9098cadfcb2513b09c42ec808d
Lounès Ksouri <dev@louneskmt.com>
2020-12-13 17:17:21 +0100

    Deleted old connect components

--- src/views/Bitcoin.vue
+++ src/views/Bitcoin.vue
@@ -59,9 +59,6 @@
               />
             </svg>
           </template>
-          <b-dropdown-item href="#" v-b-modal.connect-wallet-modal
-            >Connect Wallet</b-dropdown-item
-          >
           <b-dropdown-item href="/logs/?filter=umbrel+bitcoin" target="_blank"
             >View logs</b-dropdown-item
           >
@@ -70,119 +67,6 @@
         </b-dropdown>
       </div>
     </div>
-
-    <b-modal id="connect-wallet-modal" size="lg" centered hide-footer>
-      <template v-slot:modal-header="{ close }">
-        <div class="px-2 px-sm-3 pt-2 d-flex justify-content-between w-100">
-          <h3 class="text-lowercase">connect wallet</h3>
-          <!-- Emulate built in modal header close button action -->
-          <a href="#" class="align-self-center" v-on:click.stop.prevent="close">
-            <svg
-              width="18"
-              height="18"
-              viewBox="0 0 18 18"
-              fill="none"
-              xmlns="http://www.w3.org/2000/svg"
-            >
-              <path
-                fill-rule="evenodd"
-                clip-rule="evenodd"
-                d="M13.6003 4.44197C13.3562 4.19789 12.9605 4.19789 12.7164 4.44197L9.02116 8.1372L5.32596 4.442C5.08188 4.19792 4.68615 4.19792 4.44207 4.442C4.198 4.68607 4.198 5.0818 4.44207 5.32588L8.13728 9.02109L4.44185 12.7165C4.19777 12.9606 4.19777 13.3563 4.44185 13.6004C4.68592 13.8445 5.08165 13.8445 5.32573 13.6004L9.02116 9.90497L12.7166 13.6004C12.9607 13.8445 13.3564 13.8445 13.6005 13.6004C13.8446 13.3563 13.8446 12.9606 13.6005 12.7165L9.90505 9.02109L13.6003 5.32585C13.8444 5.08178 13.8444 4.68605 13.6003 4.44197Z"
-                fill="#6c757d"
-              />
-            </svg>
-          </a>
-        </div>
-      </template>
-      <div class="px-2 px-sm-3 pb-2 pb-sm-3">
-        <bitcoin-connect-wallet
-          :urls="{
-            p2p: onionAddress,
-            electrum: electrumAddress,
-            rpc: rpc,
-          }"
-        ></bitcoin-connect-wallet>
-      </div>
-    </b-modal>
-
-    <b-modal
-      id="connection-info-modal"
-      ref="connection-info-modal"
-      size="lg"
-      centered
-      hide-footer
-    >
-      <template v-slot:modal-header="{ close }">
-        <div class="px-2 px-sm-3 pt-2 d-flex justify-content-between w-100">
-          <h3 class="text-lowercase">connect to bitcoin core</h3>
-          <!-- Emulate built in modal header close button action -->
-          <a href="#" class="align-self-center" v-on:click.stop.prevent="close">
-            <svg
-              width="18"
-              height="18"
-              viewBox="0 0 18 18"
-              fill="none"
-              xmlns="http://www.w3.org/2000/svg"
-            >
-              <path
-                fill-rule="evenodd"
-                clip-rule="evenodd"
-                d="M13.6003 4.44197C13.3562 4.19789 12.9605 4.19789 12.7164 4.44197L9.02116 8.1372L5.32596 4.442C5.08188 4.19792 4.68615 4.19792 4.44207 4.442C4.198 4.68607 4.198 5.0818 4.44207 5.32588L8.13728 9.02109L4.44185 12.7165C4.19777 12.9606 4.19777 13.3563 4.44185 13.6004C4.68592 13.8445 5.08165 13.8445 5.32573 13.6004L9.02116 9.90497L12.7166 13.6004C12.9607 13.8445 13.3564 13.8445 13.6005 13.6004C13.8446 13.3563 13.8446 12.9606 13.6005 12.7165L9.90505 9.02109L13.6003 5.32585C13.8444 5.08178 13.8444 4.68605 13.6003 4.44197Z"
-                fill="#6c757d"
-              />
-            </svg>
-          </a>
-        </div>
-      </template>
-      <div class="px-2 px-sm-3 pb-2 pb-sm-3">
-        <div class="d-flex align-items-center">
-          <!-- Pubkey QR Code -->
-          <qr-code
-            :value="onionAddress"
-            :size="180"
-            class="qr-image mx-auto"
-            showLogo
-          ></qr-code>
-          <div class="w-100 align-self-center ml-3 ml-sm-4">
-            <p>
-              Connect your wallet to Bitcoin Core using the following address
-            </p>
-            <input-copy
-              size="sm"
-              :value="onionAddress"
-              v-if="onionAddress"
-            ></input-copy>
-            <span
-              class="loading-placeholder loading-placeholder-lg mt-1"
-              style="width: 100%"
-              v-else
-            ></span>
-          </div>
-        </div>
-        <div class="d-flex align-items-center">
-          <!-- Pubkey QR Code -->
-          <qr-code
-            :value="electrumAddress"
-            :size="180"
-            class="qr-image mx-auto"
-            showLogo
-          ></qr-code>
-          <div class="w-100 align-self-center ml-3 ml-sm-4">
-            <p>Connect your wallet to Electrum using the following address</p>
-            <input-copy
-              size="sm"
-              :value="electrumAddress"
-              v-if="electrumAddress"
-            ></input-copy>
-            <span
-              class="loading-placeholder loading-placeholder-lg mt-1"
-              style="width: 100%"
-              v-else
-            ></span>
-          </div>
-        </div>
-      </div>
-    </b-modal>
 
     <b-row class="row-eq-height">
       <b-col col cols="12" md="6" xl="4">
@@ -310,7 +194,6 @@ import QrCode from "@/components/Utility/QrCode";
 import InputCopy from "@/components/Utility/InputCopy";
 import Stat from "@/components/Utility/Stat";
 import BitcoinWallet from "@/components/BitcoinWallet";
-import BitcoinConnectWallet from "@/components/BitcoinConnectWallet";
 
 export default {
   data() {
@@ -378,7 +261,6 @@ export default {
     InputCopy,
     Stat,
     BitcoinWallet,
-    BitcoinConnectWallet,
   },
 };
 </script>
