247d2d8c974fcfa5d52506c5ce0fa113108d176d
Mayank <mayankchhabra9@gmail.com>
2020-12-25 17:28:38 +0530

    Routing scroll adjustments

--- src/router/index.js
+++ src/router/index.js
@@ -146,9 +146,20 @@ const router = new VueRouter({
   mode: "history",
   base: process.env.BASE_URL,
   routes,
-  scrollBehavior: () => {
-    return { x: 0, y: 0 };
-  } //scroll to top on page changes
+  scrollBehavior: (to, from, savedPosition) => {
+    // Exists when Browser's back/forward pressed
+    if (savedPosition) {
+      return savedPosition
+      // For anchors
+    } else if (to.hash) {
+      return { selector: to.hash, behavior: 'smooth' }
+      // By changing queries we are still in the same component, so "from.path" === "to.path" (new query changes just "to.fullPath", but not "to.path").
+    } else if (from.path === to.path) {
+      return {}
+    }
+    // Scroll to top
+    return { x: 0, y: 0 }
+  }
 });
 
 //Fake for now
