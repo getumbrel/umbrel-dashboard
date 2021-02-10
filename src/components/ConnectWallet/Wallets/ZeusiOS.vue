abcfbbdff96b0365edfb4384dfd944a88dbbdd0b
Mayank <mayankchhabra9@gmail.com>
2021-02-10 16:28:57 +0530

    Update Zeus (iOS) steps

--- src/components/ConnectWallet/Wallets/ZeusiOS.vue
+++ src/components/ConnectWallet/Wallets/ZeusiOS.vue
@@ -2,9 +2,6 @@
   <connection-details name="Zeus (iOS)">
     <step-list>
       <step>
-        Make sure your iPhone is connected to the same network as your Umbrel.
-      </step>
-      <step>
         Open Zeus on your iPhone.
       </step>
       <step>
@@ -13,32 +10,22 @@
       <step>
         Scan this QR Code (click to enlarge)
         <qr-code
-          :value="urls.lnd.restLocal"
+          :value="urls.lnd.restTor"
           :size="300"
           class="qr-image mt-2"
           showLogo
-          @click="$emit('showQrModal', urls.lnd.restLocal)"
+          @click="$emit('showQrModal', urls.lnd.restTor)"
           v-bind:style="{ cursor: 'pointer' }"
         ></qr-code>
       </step>
-    </step-list>
-    <hr />
-    <p class="text-muted">Or manually enter the following details</p>
-    <step-list>
       <step>
-        In the <span class="font-weight-bold">"Host"</span>, enter
-        <input-copy value="umbrel.local" auto-width></input-copy>
-      </step>
-      <step>
-        In the <span class="font-weight-bold">"REST Port"</span>, enter
-        <input-copy value="8080" auto-width></input-copy>
-      </step>
-      <step>
-        In the <span class="font-weight-bold">"Macaroon (Hex format)"</span>, enter 
-        <input-copy :value="macaroonHex" class="d-block mt-2"></input-copy>
+        Check <span class="font-weight-bold">"Use Tor"</span>.
       </step>
       <step>
         Tap <span class="font-weight-bold">"Save Node Config"</span>.
+      </step>
+      <step>
+        Tap <span class="font-weight-bold">"I understand, save my node config"</span>.
       </step>
       <step>
         Congratulations! You have successfully connected Zeus (iOS) to your Umbrel.
@@ -51,7 +38,6 @@
 import ConnectionDetails from "@/components/ConnectWallet/ConnectionDetails";
 import StepList from "@/components/ConnectWallet/StepList";
 import Step from "@/components/ConnectWallet/Step";
-import InputCopy from "@/components/Utility/InputCopy";
 import QrCode from "@/components/Utility/QrCode";
 
 export default {
@@ -67,7 +53,6 @@ export default {
     ConnectionDetails,
     StepList,
     Step,
-    InputCopy,
     QrCode
   }
 };
