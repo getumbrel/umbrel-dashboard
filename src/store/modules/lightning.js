19e42c993cc6d9ab1b3e7971bc142ff844b9e3de
Luke Childs <lukechilds123@gmail.com>
2021-04-09 00:55:04 +0700

    Allow Lightning API requests if we don't know it's operational

--- src/store/modules/lightning.js
+++ src/store/modules/lightning.js
@@ -218,217 +218,211 @@ const actions = {
   // These estimates have caused many customers to be confused by the numbers displayed in the dashboard (leaky sats)
   // Instead we can calculate our total balance by getting the sum of each channel's localBalance
   async getBalance({ commit, state }) {
-    if (state.operational && state.unlocked) {
-      const balance = await API.get(
-        `${process.env.VUE_APP_MIDDLEWARE_API_URL}/v1/lnd/wallet/lightning`
-      );
+    const balance = await API.get(
+      `${process.env.VUE_APP_MIDDLEWARE_API_URL}/v1/lnd/wallet/lightning`
+    );
 
-      if (balance) {
-        commit("setBalance", { confirmed: balance.balance });
-      }
+    if (balance) {
+      commit("setBalance", { confirmed: balance.balance });
     }
   },
 
   async getChannels({ commit, state }, preFetchedChannels = []) {
-    if (state.operational && state.unlocked) {
-      let rawChannels;
+    let rawChannels;
 
-      if (preFetchedChannels.length) {
-        //eg when used by lnd page
-        rawChannels = preFetchedChannels;
-      } else {
-        rawChannels = await API.get(
-          `${process.env.VUE_APP_MIDDLEWARE_API_URL}/v1/lnd/channel`
-        );
-      }
+    if (preFetchedChannels.length) {
+      //eg when used by lnd page
+      rawChannels = preFetchedChannels;
+    } else {
+      rawChannels = await API.get(
+        `${process.env.VUE_APP_MIDDLEWARE_API_URL}/v1/lnd/channel`
+      );
+    }
 
-      const channels = [];
-      let confirmedBalance = 0;
-      let pendingBalance = 0;
-      let maxReceive = 0;
-      let maxSend = 0;
+    const channels = [];
+    let confirmedBalance = 0;
+    let pendingBalance = 0;
+    let maxReceive = 0;
+    let maxSend = 0;
 
-      if (rawChannels) {
-        // Loop through channels to determine pending balance, max payment amount, and sort channels by type
-        rawChannels.forEach(channel => {
-          const localBalance = parseInt(channel.localBalance) || 0;
-          const remoteBalance = parseInt(channel.remoteBalance) || 0;
+    if (rawChannels) {
+      // Loop through channels to determine pending balance, max payment amount, and sort channels by type
+      rawChannels.forEach(channel => {
+        const localBalance = parseInt(channel.localBalance) || 0;
+        const remoteBalance = parseInt(channel.remoteBalance) || 0;
 
-          if (channel.type === "OPEN") {
-            if (channel.active) {
-              channel.status = "Online";
-            } else {
-              channel.status = "Offline";
-            }
-
-            //max receive = max remote balance in a channel
-            //max send = max local balance in a channel
-
-            // if (remoteBalance > maxReceive) {
-            //   maxReceive = remoteBalance;
-            // }
-
-            // if (localBalance > maxSend) {
-            //   maxSend = localBalance;
-            // }
-
-            maxReceive += remoteBalance;
-            maxSend += localBalance;
-
-            confirmedBalance += localBalance;
-          } else if (channel.type === "PENDING_OPEN_CHANNEL") {
-            pendingBalance += localBalance;
-            channel.status = "Opening";
-          } else if (
-            [
-              "WAITING_CLOSING_CHANNEL",
-              "FORCE_CLOSING_CHANNEL",
-              "PENDING_CLOSING_CHANNEL"
-            ].indexOf(channel.type) > -1
-          ) {
-            pendingBalance += localBalance;
-            channel.status = "Closing";
-
-            // Lnd doesn't provide initiator or autopilot data via rpc. So, we just display a generic closing message.
-            channel.name = "Closing Channel";
-            channel.purpose = "A channel that is in the process of closing";
+        if (channel.type === "OPEN") {
+          if (channel.active) {
+            channel.status = "Online";
           } else {
-            channel.status = "Unknown";
+            channel.status = "Offline";
           }
 
-          if (channel.name === "" && !channel.initiator) {
-            channel.name = "Inbound Channel";
-            channel.purpose = "A channel that another node has opened to you";
-          }
+          //max receive = max remote balance in a channel
+          //max send = max local balance in a channel
 
-          // Set placeholder values if autopilot
-          if (channel.managed === false && channel.initiator) {
-            channel.name = "Autopilot";
-            channel.purpose = "Managed by autopilot";
-          }
+          // if (remoteBalance > maxReceive) {
+          //   maxReceive = remoteBalance;
+          // }
 
-          channels.push(channel);
-        });
-        commit("setChannels", channels);
-        commit("setBalance", {
-          confirmed: confirmedBalance,
-          pending: pendingBalance
-        });
-        commit("setMaxReceive", maxReceive);
-        commit("setMaxSend", maxSend);
-      }
+          // if (localBalance > maxSend) {
+          //   maxSend = localBalance;
+          // }
+
+          maxReceive += remoteBalance;
+          maxSend += localBalance;
+
+          confirmedBalance += localBalance;
+        } else if (channel.type === "PENDING_OPEN_CHANNEL") {
+          pendingBalance += localBalance;
+          channel.status = "Opening";
+        } else if (
+          [
+            "WAITING_CLOSING_CHANNEL",
+            "FORCE_CLOSING_CHANNEL",
+            "PENDING_CLOSING_CHANNEL"
+          ].indexOf(channel.type) > -1
+        ) {
+          pendingBalance += localBalance;
+          channel.status = "Closing";
+
+          // Lnd doesn't provide initiator or autopilot data via rpc. So, we just display a generic closing message.
+          channel.name = "Closing Channel";
+          channel.purpose = "A channel that is in the process of closing";
+        } else {
+          channel.status = "Unknown";
+        }
+
+        if (channel.name === "" && !channel.initiator) {
+          channel.name = "Inbound Channel";
+          channel.purpose = "A channel that another node has opened to you";
+        }
+
+        // Set placeholder values if autopilot
+        if (channel.managed === false && channel.initiator) {
+          channel.name = "Autopilot";
+          channel.purpose = "Managed by autopilot";
+        }
+
+        channels.push(channel);
+      });
+      commit("setChannels", channels);
+      commit("setBalance", {
+        confirmed: confirmedBalance,
+        pending: pendingBalance
+      });
+      commit("setMaxReceive", maxReceive);
+      commit("setMaxSend", maxSend);
     }
   },
 
   async getTransactions({ commit, state }) {
-    if (state.operational && state.unlocked) {
-      // Get invoices and payments
-      const invoices = await API.get(
-        `${process.env.VUE_APP_MIDDLEWARE_API_URL}/v1/lnd/lightning/invoices`
-      );
-      const payments = await API.get(
-        `${process.env.VUE_APP_MIDDLEWARE_API_URL}/v1/lnd/lightning/payments`
-      );
+    // Get invoices and payments
+    const invoices = await API.get(
+      `${process.env.VUE_APP_MIDDLEWARE_API_URL}/v1/lnd/lightning/invoices`
+    );
+    const payments = await API.get(
+      `${process.env.VUE_APP_MIDDLEWARE_API_URL}/v1/lnd/lightning/payments`
+    );
 
-      if (!invoices || !payments) {
-        return;
+    if (!invoices || !payments) {
+      return;
+    }
+
+    let transactions = [];
+    let incomingTransactions = [];
+    let outgoingTransactions = [];
+
+    if (invoices) {
+      incomingTransactions = invoices.map(tx => {
+        let type = "incoming";
+        if (tx.state === "CANCELED") {
+          type = "expired";
+        } else if (tx.state === "OPEN") {
+          type = "pending";
+        }
+        return {
+          type,
+          amount: Number(tx.value),
+          timestamp: tx.settled
+            ? new Date(Number(tx.settleDate) * 1000)
+            : new Date(Number(tx.creationDate) * 1000),
+          description: tx.memo || "",
+          expiresOn: new Date(
+            (Number(tx.creationDate) + Number(tx.expiry)) * 1000
+          ),
+          paymentRequest: tx.paymentRequest
+        };
+      });
+      transactions = [...transactions, ...incomingTransactions];
+    }
+
+    if (payments) {
+      outgoingTransactions = payments.map(tx => {
+        //load tx from state to copy description
+        const preFetchedTx = state.transactions.find(
+          trx =>
+            trx.type === "outgoing" &&
+            trx.paymentPreImage === tx.paymentPreimage
+        );
+
+        return {
+          type: "outgoing",
+          amount: Number(tx.value),
+          timestamp: new Date(Number(tx.creationDate) * 1000),
+          paymentRequest: tx.paymentRequest,
+          paymentPreImage: tx.paymentPreimage,
+          fee: Number(tx.feeSat),
+          description: preFetchedTx ? preFetchedTx.description : ""
+        };
+      });
+
+      transactions = [...transactions, ...outgoingTransactions];
+    }
+
+    //Sort by recent to oldest
+    transactions.sort((tx1, tx2) => tx2.timestamp - tx1.timestamp);
+
+    //filter out new outgoing payments
+    const newOutgoingTransactions = outgoingTransactions.filter(
+      tx =>
+        !state.transactions.some(
+          trx => trx.paymentPreImage === tx.paymentPreImage
+        )
+    );
+
+    //update $store
+    commit("setTransactions", transactions);
+
+    // Fetch descriptions of all new outgoing transactions
+    for (let tx of newOutgoingTransactions) {
+      if (!tx.paymentRequest) {
+        //example - in case of a keysend tx there is no payment request
+        continue;
       }
 
-      let transactions = [];
-      let incomingTransactions = [];
-      let outgoingTransactions = [];
+      try {
+        const invoiceDetails = await API.get(
+          `${process.env.VUE_APP_MIDDLEWARE_API_URL}/v1/lnd/lightning/invoice?paymentRequest=${tx.paymentRequest}`
+        );
+        if (invoiceDetails && invoiceDetails.description) {
+          //load state's txs
+          const updatedTransactions = state.transactions;
 
-      if (invoices) {
-        incomingTransactions = invoices.map(tx => {
-          let type = "incoming";
-          if (tx.state === "CANCELED") {
-            type = "expired";
-          } else if (tx.state === "OPEN") {
-            type = "pending";
-          }
-          return {
-            type,
-            amount: Number(tx.value),
-            timestamp: tx.settled
-              ? new Date(Number(tx.settleDate) * 1000)
-              : new Date(Number(tx.creationDate) * 1000),
-            description: tx.memo || "",
-            expiresOn: new Date(
-              (Number(tx.creationDate) + Number(tx.expiry)) * 1000
-            ),
-            paymentRequest: tx.paymentRequest
-          };
-        });
-        transactions = [...transactions, ...incomingTransactions];
-      }
-
-      if (payments) {
-        outgoingTransactions = payments.map(tx => {
-          //load tx from state to copy description
-          const preFetchedTx = state.transactions.find(
-            trx =>
-              trx.type === "outgoing" &&
-              trx.paymentPreImage === tx.paymentPreimage
-          );
-
-          return {
-            type: "outgoing",
-            amount: Number(tx.value),
-            timestamp: new Date(Number(tx.creationDate) * 1000),
-            paymentRequest: tx.paymentRequest,
-            paymentPreImage: tx.paymentPreimage,
-            fee: Number(tx.feeSat),
-            description: preFetchedTx ? preFetchedTx.description : ""
-          };
-        });
-
-        transactions = [...transactions, ...outgoingTransactions];
-      }
-
-      //Sort by recent to oldest
-      transactions.sort((tx1, tx2) => tx2.timestamp - tx1.timestamp);
-
-      //filter out new outgoing payments
-      const newOutgoingTransactions = outgoingTransactions.filter(
-        tx =>
-          !state.transactions.some(
+          //find tx to update
+          const txIndex = updatedTransactions.findIndex(
             trx => trx.paymentPreImage === tx.paymentPreImage
-          )
-      );
-
-      //update $store
-      commit("setTransactions", transactions);
-
-      // Fetch descriptions of all new outgoing transactions
-      for (let tx of newOutgoingTransactions) {
-        if (!tx.paymentRequest) {
-          //example - in case of a keysend tx there is no payment request
-          continue;
-        }
-
-        try {
-          const invoiceDetails = await API.get(
-            `${process.env.VUE_APP_MIDDLEWARE_API_URL}/v1/lnd/lightning/invoice?paymentRequest=${tx.paymentRequest}`
           );
-          if (invoiceDetails && invoiceDetails.description) {
-            //load state's txs
-            const updatedTransactions = state.transactions;
 
-            //find tx to update
-            const txIndex = updatedTransactions.findIndex(
-              trx => trx.paymentPreImage === tx.paymentPreImage
-            );
-
-            if (txIndex !== -1) {
-              const outgoingTx = updatedTransactions[txIndex];
-              //update tx description and state
-              outgoingTx.description = invoiceDetails.description;
-              commit("setTransactions", transactions);
-            }
+          if (txIndex !== -1) {
+            const outgoingTx = updatedTransactions[txIndex];
+            //update tx description and state
+            outgoingTx.description = invoiceDetails.description;
+            commit("setTransactions", transactions);
           }
-        } catch (error) {
-          // console.log(error);
         }
+      } catch (error) {
+        // console.log(error);
       }
     }
   },
