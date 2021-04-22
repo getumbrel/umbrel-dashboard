f3f20e5fab7f9fc123234f57eb470586271dc41d
Luke Childs <lukechilds123@gmail.com>
2021-04-22 21:28:47 +0700

    Add Lily Wallet connection details (#338)
    Co-authored-by: Kevin Mulcrone <kaybesee@gmail.com>
    Co-authored-by: Lounès Ksouri <dev@louneskmt.com>

--- src/router/index.js
+++ src/router/index.js
@@ -29,6 +29,7 @@ import ElectrumAndroid from "../components/ConnectWallet/Wallets/ElectrumAndroid
 import ElectrumDesktop from "../components/ConnectWallet/Wallets/ElectrumDesktop.vue";
 import ElectrumServer from "../components/ConnectWallet/Wallets/ElectrumServer.vue";
 import FullyNoded from "../components/ConnectWallet/Wallets/FullyNoded.vue";
+import LilyWallet from "../components/ConnectWallet/Wallets/LilyWallet.vue";
 import Phoenix from "../components/ConnectWallet/Wallets/Phoenix.vue";
 import SamouraiWallet from "../components/ConnectWallet/Wallets/SamouraiWallet.vue";
 import Sparrow from "../components/ConnectWallet/Wallets/Sparrow.vue";
@@ -204,6 +205,13 @@ const routes = [
                 component: FullyNoded,
                 meta: {
                   wallet: "fully-noded"
+                }
+              },
+              {
+                path: "lily-wallet",
+                component: LilyWallet,
+                meta: {
+                  wallet: "lily-wallet"
                 }
               },
               {
