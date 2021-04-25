5071b8497c16e98b24121ea0f2e398c90e4b88f6
Aaron Dewes <aaron.dewes@web.de>
2021-04-25 19:20:01 +0200

    Remove orbot from Zap android
    Zap has tor builtin now.

--- src/components/ConnectWallet/Wallets/ZapAndroid.vue
+++ src/components/ConnectWallet/Wallets/ZapAndroid.vue
@@ -2,23 +2,6 @@
   <connection-details name="Zap (Android)" requires="lnd">
     <step-list>
       <step>
-        Download
-        <b-link
-          href="https://play.google.com/store/apps/details?id=org.torproject.android"
-          target="_blank"
-        >Orbot</b-link> on your phone.
-      </step>
-      <step>
-        Open Orbot, tap the gear icon under <span class="font-weight-bold">"Tor-Enabled apps"</span> and add Zap.
-      </step>
-      <step>
-        Tap <span class="font-weight-bold">"STOP"</span>, restart Orbot and turn on
-        the <span class="font-weight-bold">"VPN Mode"</span>.
-      </step>
-      <step>
-        Start Tor by tapping the big onion icon.
-      </step>
-      <step>
         Open the Zap app on your phone.
       </step>
       <step>
