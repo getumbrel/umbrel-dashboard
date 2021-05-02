44f4aed485e8889705a438a09f990d895163ed4e
Luke Childs <lukechilds123@gmail.com>
2021-05-02 19:25:26 +0700

    Add step by step instructions for Lightning

--- src/components/ConnectWallet/Wallets/BlueWallet.vue
+++ src/components/ConnectWallet/Wallets/BlueWallet.vue
@@ -2,7 +2,23 @@
   <connection-details name="BlueWallet" requires="electrum">
     <step-list>
       <step>
-        Open BlueWallet, tap the three dots to access the settings and go to
+        Install the <b-link to="/app-store/bluewallet">BlueWallet Lightning</b-link> app on your Umbrel.
+      </step>
+      <step>
+        Open BlueWallet on your phone, tap the three dots to access the settings and go to
+        <span class="font-weight-bold">Network > Lightning Settings</span>.
+      </step>
+      <step>
+        Tap <span class="font-weight-bold">"Scan or import a file"</span>.
+      </step>
+      <step>
+        Scan the QR code displayed in the <b-link to="/app-store/bluewallet">BlueWallet Lightning</b-link> app on your Umbrel.
+      </step>
+      <step>
+          Tap <span class="font-weight-bold">"Save"</span>.
+      </step>
+      <step>
+        Now go to
         <span class="font-weight-bold">Network > Electrum Server</span>.
       </step>
       <step>
@@ -26,8 +42,6 @@
         Congratulations! You have successfully connected BlueWallet to your Umbrel.
       </step>
     </step-list>
-    <hr/>
-    <p class="text-muted">For the Lightning wallets please install the <b-link to="/app-store/bluewallet">BlueWallet Lightning</b-link> app</p>
   </connection-details>
 </template>
 
