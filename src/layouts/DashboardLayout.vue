2bfbae3bab29aa1fc823ef138e2fc7b102e60e96
Luke Childs <lukechilds123@gmail.com>
2021-05-02 19:58:06 +0700

    Link to community forum instead of Telegram chat (#347)

--- src/layouts/DashboardLayout.vue
+++ src/layouts/DashboardLayout.vue
@@ -190,7 +190,7 @@
             <small>
               <a href="https://getumbrel.com" target="_blank">getumbrel.com</a>
               |
-              <a href="https://t.me/getumbrel" target="_blank">chat</a>
+              <a href="https://community.getumbrel.com" target="_blank">community</a>
             </small>
           </p>
         </footer>
