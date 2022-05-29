cab3907a7fe1a195c439f8c3d57cd3b41e0b9546
Mayank Chhabra <mayankchhabra9@gmail.com>
2022-05-29 12:18:54 +0530

    Config optimize-css plugin in prod only

--- vue.config.js
+++ vue.config.js
@@ -26,9 +26,11 @@ module.exports = {
     });
     // to disable conversation of, say, translate3d(0, 0, 0) to translateZ(0)
     // as Safari cannot render translateZ to translate3d transitions
-    config.plugin("optimize-css").tap(args => {
-      args[0].cssnanoOptions.preset[1].reduceTransforms = false;
-      return args;
-    });
+    if (process.env.NODE_ENV === "production") {
+      config.plugin("optimize-css").tap(args => {
+        args[0].cssnanoOptions.preset[1].reduceTransforms = false;
+        return args;
+      });
+    }
   }
 };
