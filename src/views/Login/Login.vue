6dcd67b24bcfa718e035afbc20fe54ad42bc82e3
Mayank Chhabra <mayankchhabra9@gmail.com>
2022-06-04 13:25:41 +0530

    /login => /start fix

--- src/views/Login/Login.vue
+++ src/views/Login/Login.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="d-flex flex-column align-items-center justify-content-center min-vh100 login-container p-2">
+  <div v-if="!loading" class="d-flex flex-column align-items-center justify-content-center min-vh100 login-container p-2">
     <logo class="mb-3 logo" />
     <h1 class="text-center text-white text-lowercase mb-1">Welcome back</h1>
     <p v-if="!showOtpInput" class="text-white w-75 text-center">Enter the password to login to your Umbrel</p>
